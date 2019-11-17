936a3d97d173d6b41b9d153fc8984ac8314eb812
Neil Shweky <neilshweky@gmail.com>
2019-11-17 17:39:20 -0500

    Initial Structure

--- Main.hs
+++ Main.hs
@@ -1,4 +1,34 @@
 module Main where
 
+-- list of top level stuff
+data Block = 
+    Block [Markdown]
+  deriving (Eq, Show)
+
+-- types of top level elements
+data Markdown = 
+    Heading Int Text
+  deriving (Eq, Show)
+
+-- A combination of strings and inlines
+data Text =
+    Text [Element]
+  deriving (Eq, Show)
+
+-- Either a string or an inline
+data Element = 
+    TextElt String 
+  | InlineElt Inline
+  deriving (Eq, Show)
+
+-- Inline with a text inside
+data Inline = 
+    Bold Text
+  | Italics Text
+  deriving (Eq, Show)
+
+
+
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
